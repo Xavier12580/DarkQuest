73b60d8e8f92658b02e9326a0b2939bb1708f6c2
Xinlong Liu <xinlongl@spinquestgpvm01.fnal.gov>
2024-08-06 19:45:55 -0500

    fixed a bug

--- e1039-analysis/DataHits/macro/gridpro_standard.sh
+++ e1039-analysis/DataHits/macro/gridpro_standard.sh
@@ -4,7 +4,7 @@ file="./run_list.txt"
 vfac=$1
 reducer_option=$2
 coarse_mode=$3
-tar -czvf public.tar.gz RecoE1039Data.C work support AnaModule setup.sh ~/test/testio/core-inst
+tar -czvf public.tar.gz RecoE1039Data.C work support AnaModule setup.sh 
 # Read the file line by line
 while IFS= read -r line
 do
